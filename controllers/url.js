b20d40f3cfc1928e77a11854c3151f2eb458666a
sumit <prajapatisumit009@gmail.com>
2024-09-08 13:17:41 +0530

    installing shortid

--- controllers/url.js
+++ controllers/url.js
@@ -1,8 +1,8 @@
-const {nanoid} = require("nanoid");
+const shortid = require("shortid");
 const URL=require("../models/url");
 
 async function handleGenerateNewShortURL(req,res){
-    const shortID=nanoid(8);
+    const shortID=shortid();
     const body=req.body;
     if(!body.url) return res.status(400).json({err:"Url is required"});
     await URL.create({
--- index.js
+++ index.js
@@ -9,4 +9,4 @@ connectToMongoDB("mongodb://localhost:27017/short-url")
 .then(()=>console.log("MongoDB connected"));
 app.use("/url",urlRoute);
 
-app.listen(port,()=>console.log(`Server started at PORT: ${PORT}`));
+app.listen(PORT,()=>console.log(`Server started at PORT: ${PORT}`));
