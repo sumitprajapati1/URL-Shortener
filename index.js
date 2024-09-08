5b144dc9d50afffb5fe52c0dcdd1dfee34647d30
sumit <prajapatisumit009@gmail.com>
2024-09-08 20:54:00 +0530

    Adding UI for frontend

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express=require("express");
 const {connectToMongoDB} =require("./config");
 const urlRoute=require("./routes/url");
+const path=require("path");
+const staticRoute=require("./routes/staticRouter");
 const URL=require("./models/url");
-
 const app=express();
-app.use(express.json());
+
 const PORT=8001;
 
 connectToMongoDB("mongodb://localhost:27017/short-url")
 .then(()=>console.log("MongoDB connected"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.set("view engine","ejs");
+app.set("views",path.resolve("./views"))
+
 app.use("/url",urlRoute);
 
-app.get("/:shortId",async (req,res)=>
+app.use("/",staticRoute);
+
+app.get("/url/:shortId",async (req,res)=>
 {
     const shortId=req.params.shortId;
     const entry = await URL.findOneAndUpdate(
